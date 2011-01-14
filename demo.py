0eaae8e999f4d2c20fd471eec52f420412fe5d10
Ashish Yadav <ashish.nopc0de@gmail.com>
2011-01-14 10:29:57 +0530

    Adding code for first working version

---
+++ demo.py
@@ -0,0 +1,22 @@
+import wsgiref.handlers
+
+import os
+from google.appengine.ext.webapp import template
+from google.appengine.ext import webapp
+
+class index(webapp.RequestHandler):
+  def get(self):
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, {}))
+
+application = webapp.WSGIApplication([
+  ('/', index),
+], debug=True)
+
+
+def main():
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+  main()
